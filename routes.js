4dca494f59b5a3a9e2b0c2dbab3bd51d2d2dcb87
CodifiedChinnappa <remotedev@myyahoo.com>
2024-10-09 06:31:06 +0530

    payment issuefixing

--- routes.js
+++ routes.js
@@ -16,6 +16,7 @@ export const publicRoutes = [
   "/our-services",
   "/cart",
   "/api/search-product",
+  "/api/search-product/by-id",
   "/api/brands",
   "/api/categories",
   "/api/testimonials",
