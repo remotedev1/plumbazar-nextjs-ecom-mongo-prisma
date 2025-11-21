8d43ad7577dba00f38d975db2c64b2d5c87fc6d9
CodifiedChinnappa <remotedev@myyahoo.com>
2025-11-21 22:57:06 +0530

    coming soon

--- components/frontend/whatsApp.jsx
+++ components/frontend/whatsApp.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const WhatsApp = () => {
   return (
-    <div className="fixed bottom-20 lg:bottom-5 right-5 bg-green-500 rounded-full p-2 h-12 w-12">
+    <div className="fixed bottom-20 lg:bottom-5 right-5 bg-green-500 rounded-full p-2 h-12 w-12 z-50">
       <Link
         href="https://wa.me/6366019800"
         className="whatsapp_float"
